42904ee3c2b4ded101215b21aa606b9f509c5af1
Julia Alves <julia.dutra@alunoescolassociais.org.br>
2024-11-07 08:23:34 -0300

    Adiciona link do API

--- moeda.js
+++ moeda.js
@@ -1,2 +1,2 @@
 const apiKey = 'fe583598e333a55873863aa4';
-const apiURL = `https://v6.exchangerate-api.com/v6/fe583598e333a55873863aa4/latest/USD${apiKey}/latest/`;
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
